2a95c8c2e0ecdee0a58cf0f588fbfe07961d6199
David Jarbas <david.jarbas97@gmail.com>
2021-10-13 20:20:26 -0300

    Primeiro commit

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({ //o $ indica JQuery
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
